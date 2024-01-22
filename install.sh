deb243bf54158ba5ece3a2967960f0ca2b94d170
Peter Arandorenko <parandorenko@gmail.com>
2024-01-21 22:15:15 -0500

    Updated install and build scripts

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo apt update
-sudo apt -y install g++
+sudo apt -y install g++ git
 sudo apt -y install python3
 sudo apt -y install python3-pip
-sudo apt -y install googletest
+sudo apt -y install libgtest-dev
 pip install cget
 cget install https://github.com/nlohmann/json/archive/refs/tags/v3.11.3.tar.gz
 cget install https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.tar.gz
