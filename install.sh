37ab523485141f1e2ca3e5003b3a3344ce06bbe7
Peter Arandorenko <parandorenko@gmail.com>
2024-01-21 21:45:23 -0500

    Added CI build and run script.

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 sudo apt update
-sudo apt install python3
-sudo apt install python3-pip
+sudo apt -y install g++
+sudo apt -y install python3
+sudo apt -y install python3-pip
+sudo apt -y install googletest
 pip install cget
 cget install https://github.com/nlohmann/json/archive/refs/tags/v3.11.3.tar.gz
 cget install https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.tar.gz
